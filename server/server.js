3cf7440b9eb7319cfa80788f840442f50c6dd35c
farmantasleem <iamfarmanmalik@gmail.com>
2023-09-15 16:12:21 +0530

    user model & db connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require("express")
-
+const connectiontoDB = require("./config/connectiontoDB")
+require("dotenv").config()
 const app = express()
 
 app.get("/",(req,res)=>{
         res.status(200).send({msg:"Server has been setup",data:"Exicited for this??"})
 })
+// connection to db
 
+connectiontoDB()
 app.listen(8081,()=>{
     console.log("Server has started Successfully")
 })
